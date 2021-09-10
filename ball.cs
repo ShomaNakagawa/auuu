803b05bddefab58fd628a0eeee707c12d019f27d
ShomaNakagawa <b1019176@fun.ac.jp>
2021-09-10 10:15:56 +0900

    Create ball.cs

---
+++ ball.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody myRigid;
+    public GameManeger mymanager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRigid = this.GetComponent<Rigidbody>();
+       myRigid.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+private void OnCollisionEnter(Collision collision)
+{
+    if( collision.gameObject.tag == "Finish")
+    {
+        Destroy(this.gameObject);
+        mymanager.GameOver();
+    }
+}
+
+}
